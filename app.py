2651bff156d1606f4b80fe496a709e77f8d3296b
Mrityunjay Kumar <mjay.cse@gmail.com>
2018-03-20 19:17:52 +0530

    new app

--- app.py
+++ app.py
@@ -5,36 +5,22 @@ from flask import Flask
 from nikola.plugins.command.serve import OurHTTPRequestHandler
 
 
-class FlaskNew(Flask):
-    def __init__(self, import_name):
-        super().__init__(import_name)
-
-    def run(self, host=None, port=None, debug=None, **options):
-        def execute(**options):
-            """Start test server."""
-            out_dir = 'output'
-            # if os.path.exists(out_dir) is False:
-            #     os.system('nikola build')
-            if not os.path.isdir(out_dir):
-                print("MJAY:\t Error: Missing '{0}' folder?".format(out_dir))
-            else:
-                os.chdir(out_dir)
-                port = options['port']
-                if port is None:
-                    port = 8000
-                httpd = HTTPServer((options['address'], port),
-                                   OurHTTPRequestHandler)
-                sa = httpd.socket.getsockname()
-                print("Serving HTTP on", sa[0], "port", sa[1], "...")
-                httpd.serve_forever()
-
-        execute(address=host, port=port)
-        # super().run(host,port,debug, **options)
-
-
-app = FlaskNew(__name__)
+def execute(**kwargs):
+    """Start test server."""
+    out_dir = 'output'
+    # if os.path.exists(out_dir) is False:
+    #     os.system('nikola build')
+    if not os.path.isdir(out_dir):
+        print("MJAY:\t Error: Missing '{0}' folder?".format(out_dir))
+    else:
+        os.chdir(out_dir)
+        port = kwargs.get('port', 8000)
+        httpd = HTTPServer((kwargs['address'], port),
+                           OurHTTPRequestHandler)
+        sa = httpd.socket.getsockname()
+        print("Serving HTTP on", sa[0], "port", sa[1], "...")
+        httpd.serve_forever()
 
 
 if __name__ == "__main__":
-    os.system('nikola build')
-    app.run(host="127.0.0.1")
+    execute(address="127.0.0.1")
